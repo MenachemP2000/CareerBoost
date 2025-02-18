6fe9f46dd5dd60b8d4f070a9d61bb113d16b7244
Menachem Parente <74961803+MenachemP2000@users.noreply.github.com>
2025-02-18 11:08:53 +0200

    Improved the model

--- Model/model_preprocess.py
+++ Model/model_preprocess.py
@@ -1,10 +1,11 @@
 import pandas as pd
-dtf = pd.read_csv("./survey_results_minimal.csv",index_col='ResponseId', encoding='utf-8')
-employments = dtf['Employment'].str.split(';', expand=True).stack().unique()
-print(employments)
+dtf = pd.read_csv("./survey_results.csv",index_col='ResponseId', encoding='utf-8')
+dtf= dtf.fillna("NoDataBase")
+DatabaseHaveWorkedWith = dtf['DatabaseHaveWorkedWith'].str.split(';', expand=True).stack().unique()
+print(DatabaseHaveWorkedWith)
 
 export = pd.DataFrame(index=dtf.index)
-for employment in employments:
-    dtf[employment] = dtf['Employment'].str.contains(employment).fillna(False).astype(int)
-    export[employment] = dtf[employment]
+for Database in DatabaseHaveWorkedWith:
+    dtf[Database] = dtf['DatabaseHaveWorkedWith'].str.contains(Database).fillna(False).astype(int)
+    export[Database] = dtf[Database]
 export.to_csv('model_preprocess.csv', index=True, header=True)
